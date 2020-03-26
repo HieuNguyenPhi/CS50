e2053c7aac92da655fcbcfdef5a64f6c5547f7c3
HieuNguyenPhi <hieunguyenphi94@gmail.com>
2020-03-26 13:56:31 +0700

    Update readability.c

--- Week 2/readability.c
+++ Week 2/readability.c
@@ -1,50 +1,49 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
-#include <math.h>
 
 float grade(int l, int w, int s);
 
 int main(void)
 {
     string userinput = get_string("Text :\n");
-    //int i;
-    int letter = 0, word = 0, sentence = 0;
-    for (int i=0; i < strlen(userinput); i++)
+    int letter = 0, word = 1, sentence = 0;
+    //counting
+    for (int i = 0; i < strlen(userinput); i++)
     {
-        if ((userinput[i]>='A' && userinput[i] <= 'Z') || (userinput[i] >='a' && userinput[i]<='z'))
+        if ((userinput[i] >= 'A' && userinput[i] <= 'Z') || (userinput[i] >= 'a' && userinput[i] <= 'z'))
         {
             letter++;
         }
-        else if(userinput[i]==' ')
+        else if (userinput[i] == ' ')
         {
             word++;
         }
-        else if(userinput[i] =='.' || userinput[i] =='?' || userinput[i] == '!')
+        else if (userinput[i] == '.' || userinput[i] == '?' || userinput[i] == '!')
         {
             sentence++;
         }
     }
-    printf("%i letters, %i words, %i sentences.\n",letter, word+1, sentence);
-    float index = grade(letter,word+1,sentence);
-    if (index >=16)
+    //grading
+    float index = grade(letter, word, sentence);
+    if (index >= 16)
     {
         printf("Grade 16+");
     }
-    else if(index < 1)
+    else if (index < 1)
     {
         printf("Before Grade 1");
     }
     else
     {
-        printf("Grade %i", (int) round(index));
+        printf("Grade %i", (int)(index + 0.5));
     }
     printf("\n");
 }
 
 float grade(int l, int w, int s)
 {
-    float L = 100*l/w;
-    float S = 100*s/w;
+    float L = 100 * (float)l / (float)w;
+    float S = 100 * (float)s / (float)w;
     return 0.0588 * L - 0.296 * S - 15.8;
 }
